f6a0dd2bc54fe70daa1f40fc339f6d7a7b8b5730
Pravin-Hexaware <2000160744@hexaware.com>
2025-09-10 09:23:01 +0530

    Update UseCase.py

--- Day2_Assignments/banking/UseCase5.py
+++ Day2_Assignments/banking/UseCase5.py
@@ -1,10 +1,7 @@
 import accounts as acc
 import transactions as tr
 
-acc.createAccount(101,"amal",1000)
-
+acc.createAccount(101,"pravin",1000)
 tr.deposit(101,500)
-
 tr.withdraw(101,200)
-
 print("balance:",acc.getBalance(101))
