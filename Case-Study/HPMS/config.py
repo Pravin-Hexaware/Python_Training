e64e170fc4c95fca52f6626fb03e957d0af01987
Pravin-Hexaware <2000160744@hexaware.com>
2025-09-14 18:05:14 +0530

    Update config.py

--- Case-Study/HPMS/config.py
+++ Case-Study/HPMS/config.py
@@ -1,6 +1,6 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI="mysql+pymysql://root:@localhost/patient_management"
+    SQLALCHEMY_DATABASE_URI="mysql+pymysql://root:root@localhost/PMS"
     SQLALCHEMY_TRACK_MODIFICATIONS=False
     SECRET_KEY=os.getenv("SECRET_KEY","my_secret_key")
